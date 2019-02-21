ab15cbc257e7b5e06d600deb3f28bad455a2ada2
JakeTrapnell <trapnelljake@gmail.com>
2019-02-21 15:01:11 +0000

    +find trainee by login service +app.prop changes

--- src/main/java/com/qa/InspectorFacade/service/TraineeServiceImpl.java
+++ src/main/java/com/qa/InspectorFacade/service/TraineeServiceImpl.java
@@ -1,7 +1,9 @@
 package com.qa.InspectorFacade.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,43 +14,59 @@ import com.qa.InspectorFacade.persistence.repository.TraineeRepo;
 
 @Service
 public class TraineeServiceImpl implements TraineeService {
-	
+
 	@Autowired
 	private TraineeRepo repo;
-	
+
 	public List<Trainee> getTrainees() {
 		return repo.findAll();
 	}
-	
+
+	public ResponseEntity<Object> getTraineeByLogin(String email, String password) {
+
+		ArrayList<Trainee> allTrainees = (ArrayList<Trainee>) repo.findAll();
+		ArrayList<Trainee> theTrainee = new ArrayList<Trainee>();
+
+		theTrainee = (ArrayList<Trainee>) allTrainees.stream()
+				.filter(x -> (email.equals(x.getEmail())) && password.equals(x.getPassword()))
+				.collect(Collectors.toList());
+
+		if (theTrainee.size() == 1) {
+			repo.save(theTrainee.get(0));
+			return ResponseEntity.ok().build();
+		} 
+		else {
+
+		return ResponseEntity.noContent().build();
+		}
+	}
+
 	public Trainee createTrainee(Trainee trainee) {
 		return repo.save(trainee);
 	}
-	
+
 	public ResponseEntity<Object> deleteTrainee(Long id) {
 		if (traineeExists(id)) {
 			repo.deleteById(id);
 			return ResponseEntity.ok().build();
-		}
-		else {
+		} else {
 			return ResponseEntity.notFound().build();
 		}
 	}
-	
+
 	public ResponseEntity<Object> updateTrainee(Trainee trainee, Long id) {
 		if (traineeExists(id)) {
 			trainee.setTraineeId(id);
 			repo.save(trainee);
 			return ResponseEntity.ok().build();
-		}
-		else {
+		} else {
 			return ResponseEntity.notFound().build();
 		}
 	}
-	
-	private boolean traineeExists(Long id){
-        Optional<Trainee> traineeOptional = repo.findById(id);
-        return traineeOptional.isPresent();
-    }
-	
+
+	private boolean traineeExists(Long id) {
+		Optional<Trainee> traineeOptional = repo.findById(id);
+		return traineeOptional.isPresent();
+	}
 
 }
